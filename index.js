3846473983e270d52564b7843e620b06ca27c901
Suraj <2015suraj.pawar@ves.ac.in>
2020-04-18 00:46:55 -0400

    Enough for today

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 addEventListener('fetch', event => {
 	event.respondWith(handleRequest(event.request))
 })
@@ -5,75 +6,6 @@ addEventListener('fetch', event => {
  * Respond with hello worker text
  * @param {Request} request
  */
-//CHALLENGE COMPLETED BY MAULIK SHAH 
-//URL: https://maulik-app.fullstack-challenge.workers.dev/
-
-async function handleRequest(request) {
-	return fetch('https://cfw-takehome.developers.workers.dev/api/variants') //request to the API
-		.then(res => {
-			if (res.ok) {
-				return res.json();
-			} else {
-				throw new Error(res.status);
-			}
-		})
-		.then(res => {
-			let urls = res.variants;  //get the urls as an array
-
-			//we need to first check if the user visited the variant
-			//initially the person won't, however, we need to check 
-			// so next time the person goes to the webapp the 
-			// person will go to the variant that they visited before
-			let decision;
-			let cookie = request.headers.get('Cookie') //request cookie
-			if (cookie && cookie.includes('variant=#1')) {
-				decision = true
-			} else if (cookie && cookie.includes('variant=#2')) {
-				decision = false
-			} else {
-				decision = (Math.random() < 0.5); //this is for 50/50 chance
-			}
-			if (decision == true) {
-				return fetchVariant(urls[0], decision)
-			} else {
-				return fetchVariant(urls[1], decision)
-			}
-		}).catch(error => {
-			console.error(error);
-		});
-}
-
-async function fetchVariant(req, decision) {
-	let res = await fetch(req); //request to one of the urls
-	var expiryDate = new Date()
-	expiryDate.setDate(expiryDate.getDate() + 4); //expires in 4 days
-	if (res.ok) {
-		if (decision) { //manipulate the html depending on the variant
-			res = new HTMLRewriter()
-				.on('p#description', new ElementHandler('Aspiring Software Engineer... I also love to design!'))
-				.on('h1#title', new ElementHandler('Cool Challenge #1'))
-				.on('a#url', new ElementHandler('My Portfolio'))
-				.on('title', new ElementHandler('Heyyy!!'))
-				.on('a', new myAttributeHandler('href', 'https://www.mauliks.me/'))
-				.transform(res)
-			res.headers.append('Set-Cookie', `variant=#1; Secure; Expiry=${expiryDate.toGMTString()}`)
-			return res;
-		} else {
-			res = new HTMLRewriter()
-				.on('p#description', new ElementHandler('I love to empower students about tech! '))
-				.on('h1#title', new ElementHandler('Cool Challenge #2'))
-				.on('a#url', new ElementHandler('My Linkedin'))
-				.on('title', new ElementHandler('Yayyy!!'))
-				.on('a', new myAttributeHandler('href', 'https://www.linkedin.com/in/maulikshah87/'))
-				.transform(res)
-			res.headers.append('Set-Cookie', `variant=#2; Secure; Expiry=${expiryDate.toGMTString()}`)
-			return res;
-		}
-	} else {
-		throw new Error(res.status);
-	}
-}
-
 class ElementHandler {
 	constructor(myText) {
 		this.myText = myText
@@ -82,8 +14,7 @@ class ElementHandler {
 		element.setInnerContent(this.myText)
 	}
 }
-
-class myAttributeHandler {
+class myURL {
 	constructor(attributeName, link) {
 		this.attributeName = attributeName
 		this.link = link
@@ -98,3 +29,28 @@ class myAttributeHandler {
 		}
 	}
 }
+async function handleRequest(request) {
+	try {
+		let response = await fetch('https://cfw-takehome.developers.workers.dev/api/variants');
+		let rand = Math.floor(2 * Math.random())
+		let data = await response.json();
+		let request = new Request(data['variants'][rand])
+		let htmlwriter = await fetch(request)
+		htmlwriter = new Response(htmlwriter.body, htmlwriter)
+		var expiryDate = new Date()
+		expiryDate.setDate(expiryDate.getDate() + 4); //expires in 4 days
+
+		htmlwriter = new HTMLRewriter()
+			.on('p', new ElementHandler("Hello"))
+			.on('a#url', new myURL("href", "https://ssp4all.github.io/"))
+			.on('h1', new ElementHandler("Cloudflare challenge 2020"))
+			.transform(htmlwriter)
+		htmlwriter.headers.append('Set-Cookie', `variant=#1; Secure; Expiry=${expiryDate.toGMTString()}`)
+
+		return htmlwriter
+	}
+	catch (e) {
+		console.log("Err >> " + e)
+	}
+}
+
